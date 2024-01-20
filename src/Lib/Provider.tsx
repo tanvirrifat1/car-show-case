e1bf1189e88c8a2d1bf0432c90616782b9b67475
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-01-20 19:03:13 +0600

    setup redux

--- src/Lib/Provider.tsx
+++ src/Lib/Provider.tsx
@@ -3,9 +3,15 @@ import React from "react";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { IChildrenProps } from "@/Type/Common";
+import { Provider } from "react-redux";
+import { store } from "@/Redux/store";
 
-const Providers = ({ children }: { children: IChildrenProps }) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </Provider>
+  );
 };
 
 export default Providers;
---
+++ src/Redux/store.ts
@@ -0,0 +1,10 @@
+import { reducer } from "./rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
